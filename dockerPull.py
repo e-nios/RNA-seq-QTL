a245a752c4135e8fbcb859709e63827379b7ca17
MEBP-NHRF <37841234+MEBP-NHRF@users.noreply.github.com>
2018-03-28 17:00:36 +0300

    Update dockerPull.py

--- dockerPull.py
+++ dockerPull.py
@@ -2,12 +2,12 @@
 
 import subprocess
 
-subprocess.check_call(['docker', 'pull', 'mebp/qtl-rnaseq-workflow:trim-galore'])
+subprocess.check_call(['docker', 'pull', 'enios/rnaseq-qtl:trim-galore'])
 
-subprocess.check_call(['docker', 'pull', 'mebp/qtl-rnaseq-workflow:hisat2'])
+subprocess.check_call(['docker', 'pull', 'enios/rnaseq-qtl:hisat2'])
 
-subprocess.check_call(['docker', 'pull', 'mebp/qtl-rnaseq-workflow:featurecounts'])
+subprocess.check_call(['docker', 'pull', 'enios/rnaseq-qtl:featurecounts'])
 
-subprocess.check_call(['docker', 'pull', 'mebp/qtl-rnaseq-workflow:edger'])
+subprocess.check_call(['docker', 'pull', 'enios/rnaseq-qtl:edger'])
 
-subprocess.check_call(['docker', 'pull', 'mebp/qtl-rnaseq-workflow:happy'])
+subprocess.check_call(['docker', 'pull', 'enios/rnaseq-qtl:happy'])
